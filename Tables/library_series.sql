9c23684fba2cafc112703c9358991f18be2ea7b7
Joshua Weber <63745585+JWebCoding@users.noreply.github.com>
2025-04-18 22:38:29 -0500

    Backup upload

--- Tables/library_series.sql
+++ Tables/library_series.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.31, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.33, for Linux (x86_64)
 --
 -- Host: localhost    Database: library
 -- ------------------------------------------------------
--- Server version	8.0.31
+-- Server version	8.0.33
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -26,7 +26,7 @@ CREATE TABLE `series` (
   `seriesID` int NOT NULL AUTO_INCREMENT,
   `series_name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`seriesID`)
-) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -35,7 +35,7 @@ CREATE TABLE `series` (
 
 LOCK TABLES `series` WRITE;
 /*!40000 ALTER TABLE `series` DISABLE KEYS */;
-INSERT INTO `series` VALUES (0,''),(1,'Decline and Fall'),(4,'War and Peace'),(5,'Harry Potter Deutsch'),(6,'Chronicles of Prydain'),(7,'Bone'),(8,'A Series of Unfortunate Events'),(9,'Readers Bible'),(10,'The Lord of the Rings'),(11,'A Song of Ice and Fire'),(12,'Liberation Trilogy'),(13,'Theodore Roosevelt'),(14,'The Art of Computer Programming'),(15,'CCNA Cert Guide'),(16,'Harry Potter'),(17,'I Am Number 4'),(18,'To All the Boys Ive Loved Before'),(19,'Left Behind'),(20,'Delirium'),(21,'Vampire Diaries - The Salvation'),(22,'The Vampire Diaries - The Return'),(23,'The Vampire Diaries - The Hunters'),(24,'The Vampire Diaries - Stefans Diaries'),(25,'The Vampire Diaries'),(26,'Inheritance Cycle'),(27,'Maze Runner'),(28,'Twilight'),(29,'The Mortal Insturments'),(30,'An Ember Against the Night'),(31,'The Princess Diaries'),(32,'Red Queen'),(33,'Summer'),(34,'Unearthly'),(35,'Harry Potter British'),(36,'Shiloh'),(37,'Bridgerton'),(38,'The Revolution Trilogy'),(39,'Locke & Key'),(40,'Mark Twains Works'),(42,'The Dark Artifices'),(43,'Harvard Classics'),(44,'test series'),(45,'Halo'),(46,'The Witcher'),(47,'The Vampire Chronicles'),(48,'Journey to the West');
+INSERT INTO `series` VALUES (0,''),(1,'Decline and Fall'),(4,'War and Peace'),(5,'Harry Potter Deutsch'),(6,'Chronicles of Prydain'),(7,'Bone'),(8,'A Series of Unfortunate Events'),(9,'Readers Bible'),(10,'The Lord of the Rings'),(11,'A Song of Ice and Fire'),(12,'Liberation Trilogy'),(13,'Theodore Roosevelt'),(14,'The Art of Computer Programming'),(15,'CCNA Cert Guide'),(16,'Harry Potter'),(17,'I Am Number 4'),(18,'To All the Boys Ive Loved Before'),(19,'Left Behind'),(20,'Delirium'),(21,'Vampire Diaries - The Salvation'),(22,'The Vampire Diaries - The Return'),(23,'The Vampire Diaries - The Hunters'),(24,'The Vampire Diaries - Stefans Diaries'),(25,'The Vampire Diaries'),(26,'Inheritance Cycle'),(27,'Maze Runner'),(28,'Twilight'),(29,'The Mortal Insturments'),(30,'An Ember Against the Night'),(31,'The Princess Diaries'),(32,'Red Queen'),(33,'Summer'),(34,'Unearthly'),(35,'Harry Potter British'),(36,'Shiloh'),(37,'Bridgerton'),(38,'The Revolution Trilogy'),(39,'Locke & Key'),(40,'Mark Twains Works'),(42,'The Dark Artifices'),(43,'Harvard Classics'),(44,'test series'),(45,'Halo'),(46,'The Witcher'),(47,'The Vampire Chronicles'),(48,'Journey to the West'),(49,'The Twice Dead King'),(50,'Vampire Academy'),(51,'Berserk Deluxe'),(52,'A Court of Thorns and Roses'),(53,'test');
 /*!40000 ALTER TABLE `series` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -48,4 +48,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-12-05  1:36:35
+-- Dump completed on 2023-04-25 19:42:20
--- Tables/library_user.sql
+++ Tables/library_user.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.31, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.33, for Linux (x86_64)
 --
 -- Host: localhost    Database: library
 -- ------------------------------------------------------
--- Server version	8.0.31
+-- Server version	8.0.33
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -50,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-12-05  1:36:35
+-- Dump completed on 2023-04-25 19:42:20
